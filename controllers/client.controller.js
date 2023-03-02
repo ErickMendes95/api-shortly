a1632da8200c02d5cb0066181c80a2b2c59c18dd
Erick Mendes <erickmendesweb@gmail.com>
2023-03-02 17:49:43 -0300

    fix: rota ranking

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -97,7 +97,7 @@ export async function ranking(req,res){
         SUM("shortenedUrls"."visitCount") AS "visitCount" 
         from users left join "shortenedUrls" on "shortenedUrls"."userId"= users.id
         group by users.id 
-        order by visit_count desc limit 10`);
+        order by "visitCount" desc limit 10`);
 
         res.status(200).send(users.rows);
         
