efe07f1a38359504f80d71dcba377a5169d3aa81
Erick Mendes <erickmendesweb@gmail.com>
2023-03-02 15:02:05 -0300

    fix: start do server

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import router from '../routers/app.router';
+import router from '../routers/app.router.js';
 
 dotenv.config();
 
